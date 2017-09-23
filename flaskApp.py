d5781117e9ec6e0bbb5ad2f77995339371c56991
carlogilmar <carlogilmar12@gmail.com>
2017-09-22 23:50:27 -0500

    Agregando primera app usando flask

---
+++ flaskApp.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello, Flask!'
+
+@app.route('/hello/<name>')
+def hello(name):
+    return '<h1>Hello</h1>, %s' % name
+
+# following lines execute our app using the built-in development server in debug mode
+if __name__ == '__main__':
+    app.run(debug=True)
+
